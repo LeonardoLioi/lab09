e7c6c4b5370c65495f900de754766f313223a268
Leonardo Lioi <leonardo.lioi@studio.unibo.it>
2025-11-18 20:26:52 +0100

    parte 2 completata al 50%

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,40 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+     private File currentFile;
+     private static final String PATH = System.getProperty("user.home")
+            + File.separator
+            + "output.txt";
 
+     public Controller() {
+        this.currentFile = new File(PATH);
+    }
+
+    public void setFile(File file){
+        this.currentFile = file;
+
+    }
+    public File getFile(){
+        return this.currentFile;
+    }
+    public String getFilePath(){
+        return this.currentFile.getPath();
+    }
+    public void writeContent(String text){
+
+        try {
+                Files.writeString(Path.of(this.currentFile.getPath()), text);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+    }
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,12 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.border.Border;
+
+import java.awt.BorderLayout;
 
 /**
  * A very simple program using a graphical interface.
@@ -10,4 +16,35 @@ public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
 
+    public SimpleGUI() {
+        frame.setTitle("My first graphical interface");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 400);
+
+        JPanel jp = new JPanel();
+        jp.setLayout(new BorderLayout());
+        JTextArea text = new JTextArea();
+        JButton b = new JButton("Save");
+        
+        jp.add(text, BorderLayout.CENTER);
+        jp.add(b, BorderLayout.SOUTH);
+        
+	    frame.getContentPane().add(jp);
+        frame.setVisible(true);
+
+    }
+
+    public void start(){
+        // new SimpleGUI();
+
+    }
+
+    public static void main(final String ...args){
+
+        new SimpleGUI().start();
+
+    }
+
+
 }
+
